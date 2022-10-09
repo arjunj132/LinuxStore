65f8c9e2f07fa13a73182859a03e3cabeef20237
Arjun Jakkipally <68869672+arjunj132@users.noreply.github.com>
2022-10-09 14:02:21 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,2 @@
-console.log("LinuxStore is powered by snapcraft, npm, and pypi (pip)")
+console.log("If the fetch errors, do not pay attention. It is so when checking apps, runner is all set to scrape")
+fetch("https://lsapi.arjunjakkipally.repl.co")
